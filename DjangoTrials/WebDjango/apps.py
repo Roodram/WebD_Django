7a3a309144b8b09ab3d5362b575cdbdfc80926a5
Roodram <paneriroodram@gmail.com>
2018-09-03 22:37:05 +0530

    Django Files

---
+++ DjangoTrials/WebDjango/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WebdjangoConfig(AppConfig):
+    name = 'WebDjango'
---
+++ DjangoTrials/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/media/roodram/72F8C754F8C7156F/PycharmProjects/DjangoTrials/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
